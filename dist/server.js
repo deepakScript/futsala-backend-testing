07dd8725c9e583fbccda5eba3fbe4fe6188a6ee8
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2026-01-17 13:52:18 +0545

    fix

--- dist/server.js
+++ dist/server.js
@@ -60,9 +60,7 @@ app.use((err, req, res, next) => {
     });
 });
 const PORT = process.env.PORT ?? 5000;
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Global unhandled promise rejection handler
 process.on("unhandledRejection", (reason, promise) => {
     console.error("Unhandled Rejection at:", promise, "reason:", reason);
--- src/server.ts
+++ src/server.ts
@@ -66,9 +66,7 @@ app.use((err: any, req: Request, res: Response, next: any) => {
 
 const PORT = process.env.PORT ?? 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Global unhandled promise rejection handler
 process.on("unhandledRejection", (reason, promise) => {
